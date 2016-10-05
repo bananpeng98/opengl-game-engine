676917069b6375d2316d29b10715d2522a2183da
bananpeng98 <eric.norman98@hotmail.se>
2016-10-05 19:58:47 +0200

    Added class library

--- opengl-game-engine/Program.cs
+++ opengl-game-engine/Program.cs
@@ -4,12 +4,13 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace opengl_game_engine
+namespace testing
 {
     class Program
     {
         static void Main(string[] args)
         {
+
         }
     }
 }
